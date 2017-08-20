6173b8345539a748be47a7d22bf6290ea5ae3f74
simonhkswan <shks2@cam.ac.uk>
2017-08-20 12:23:36 +0100

    fixed initial errors

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "If you can see this. Its working!"
 
 if __name__ == "__main__":
-    app.run()
+    application.run()

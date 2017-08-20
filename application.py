7d96ec1880d55829a7df11150214e71a6a22db62
simonhkswan <shks2@cam.ac.uk>
2017-08-20 11:56:44 +0100

    fixed typo

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print("If you can see this. Its working!")
+    return "If you can see this. Its working!"
 
 if __name__ == "__main__":
-    application.run()
+    app.run()

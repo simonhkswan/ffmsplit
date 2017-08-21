54feb8787c223e694082784bb3d251fbe4db62c4
simonhkswan <shks2@cam.ac.uk>
2017-08-21 05:19:43 +0100

    new uploading attempt

--- application.py
+++ application.py
@@ -1,55 +1,25 @@
-import os
-from flask import Flask, request, redirect, url_for
-from flask_s3 import FlaskS3
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, flash
+from flask_wtf import Form
+from flask_wtf.file import FileField
+from tools import s3_upload
 
-UPLOAD_FOLDER = '/tmp/'
-ALLOWED_EXTENSIONS = set(['txt', 'mp4'])
-
-
-s3 = FlaskS3()
-application = api = Flask(__name__)
-s3.init_app(api)
-api.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@api.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-@api.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+app = Flask(__name__)
+app.config.from_object('config')
 
 
-if __name__ == "__main__":
-    application.run()
+class UploadForm(Form):
+    example = FileField('Example File')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def upload_page():
+    form = UploadForm()
+    if form.validate_on_submit():
+        output = s3_upload(form.example)
+        flash('{src} uploaded to S3 as {dst}'.format(src=form.example.data.filename, dst=output))
+    return render_template('example.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
     
